b5795e75870b990fd32b991f172284b553b4c33d
ElliotLard <el1458@cs.ship.edu>
2016-04-25 08:59:25 -0400

    retrieved the test folder

---
+++ Roguelike/test/lifeform/MockLifeForm.java
@@ -0,0 +1,10 @@
+package lifeform;
+
+import environment.Cell;
+
+public class MockLifeForm extends LifeForm
+{
+	public MockLifeForm(String r, String n, int s, Cell l){
+		super(r, n, s, l);
+	}
+}
