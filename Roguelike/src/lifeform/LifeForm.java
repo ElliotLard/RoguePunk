f1f4f21c843dbae7290022e9a0d0d0b71778d86b
Chris <Chris@Chris-PC>
2016-04-26 13:38:29 -0400

    updated: Moved move command because I like to move it move it.

--- Roguelike/src/lifeform/LifeForm.java
+++ Roguelike/src/lifeform/LifeForm.java
@@ -77,30 +77,7 @@ public abstract class LifeForm implements RoundObserver
 	public void updateStats(){
 		
 	}
-	
-	/**
-	 * In theory, this is getting a keyboard value in
-	 * int form and will move in whatever direction.
-	 * @param key
-	 */
-	public void move(int key){
-		if (key == 224) // 224 Value for UP
-		{
-			
-		}
-		else if (key == 225) // 225 Value for Down
-		{
-			
-		}
-		else if (key == 226) // 226 Value for Left
-		{
-			
-		}
-		else if (key == 227) // 227 Value for Right
-		{
-			
-		}
-	}
+
 	
 	/**
 	 * Generic damage calculation
@@ -117,5 +94,11 @@ public abstract class LifeForm implements RoundObserver
 		if (round % 1 == 0){
 			actionPoints[0] = actionPoints[1];
 		}
+	}
+
+	public void move(int key)
+	{
+		// TODO Auto-generated method stub
+		
 	}
 }
